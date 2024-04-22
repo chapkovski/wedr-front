5c259037909514e752c8144b2bb5f5cf3c53c66c
Philipp Chapkovski <chapkovski@gmail.com>
2024-04-22 15:49:40 +0200

    first movement to timeout intro and some rerewrint to make code cleaner

--- src/store.js
+++ src/store.js
@@ -17,23 +17,28 @@ export const useWebSocketStore = defineStore({
       this.messages.push(newMessage); // Add message data to the messages array
     },
     handle_completed(newMessage) {
-      const {group_completed, who} = newMessage;
+      const { group_completed, who, new_data, timeOver} = newMessage;
       if (group_completed) {
-        this.allowedToProceed = true;
-        // submit a #form
-        document.getElementById('form').submit();
+        if (timeOver) {
+          this.allowedToProceed = true;
+          document.getElementById('form').submit();
+          return
+        }
+        if (new_data) {
+          this.data = new_data;
+        }
       }
     },
     initializeWebSocket() {
-      const that=this;
+      const that = this;
       const { status, data, send } = useWebSocket(window.fullPath, {
         autoReconnect: true,
         onMessage: (e) => {
           const json_data = JSON.parse(data.value);
           console.debug("Message received!", json_data);
           this.data = data.value;
-          
-          if (json_data ) {
+
+          if (json_data) {
             const newMessage = json_data;
             // Dynamically call the appropriate handler based on the message type
             const handlerName = `handle_${newMessage.type}`;
@@ -47,19 +52,19 @@ export const useWebSocketStore = defineStore({
         onConnected: async () => {
           console.debug("Connected!");
           that.status = 'connected';
-          
-          
+
+
         },
       });
       that.status = status;
-      
+
 
     },
     async sendMessage(type, data) {
       // Use the 'send' function from the state
       liveSend({
         type, data
-    })
+      })
     },
   },
 });
